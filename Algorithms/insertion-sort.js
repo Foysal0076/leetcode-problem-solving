b2fcd3dfbc28eb75d874837c40b1e03f5e77f60e
Foysal Ahmed <foysalxahmed@gmail.com>
2024-06-12 15:32:20 +0600

    Add insertion sort

--- Algorithms/insertion-sort.js
+++ Algorithms/insertion-sort.js
@@ -1,9 +1,24 @@
-const insertionSort = (nums) => {}
+const insertionSort = (nums) => {
+  if (nums.length <= 1) return nums
+  for (let i = 1; i < nums.length; i++) {
+    let left = i - 1
+    let current = i
+    while (nums[current] < nums[left] && left >= 0) {
+      // swap number
+      nums[left] = nums[left] + nums[current]
+      nums[current] = nums[left] - nums[current]
+      nums[left] = nums[left] - nums[current]
+      left--
+      current--
+    }
+  }
+  return nums
+}
 
 const test1 = [50]
 const test2 = [4, 5, 6, 7, 8, 2, 1]
 const test3 = [11, 2, 83, 40, 50, 6, 7, 8, 9]
-const test5 = []
+const test4 = []
 
 console.log(insertionSort(test1))
 console.log(insertionSort(test2))
