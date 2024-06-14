504a8723ceeed21007655e72713f524dae3ac596
Foysal Ahmed <foysalxahmed@gmail.com>
2024-06-14 10:45:21 +0600

    Add quick sort

--- Algorithms/quick-sort.js
+++ Algorithms/quick-sort.js
@@ -1,9 +1,23 @@
-const quickSort = (nums) => {}
+const quickSort = (nums) => {
+  if (nums.length < 2) return nums
+  const pivot = nums[nums.length - 1]
+  const left = []
+  const right = []
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i])
+    } else {
+      right.push(nums[i])
+    }
+  }
+  const res = [...quickSort(left), pivot, ...quickSort(right)]
+  return res
+}
 
 const test1 = [50]
-const test2 = [4, 5, 6, 7, 8, 2, 1]
-const test3 = [11, 2, 83, 40, 50, 6, 7, 8, 9]
-const test5 = []
+const test2 = [4, 5, 3, 7, 6, 2, 1]
+const test3 = [5, 2, 1, 4, 0, 3, 6, 8, 7, 4, -1]
+const test4 = []
 
 console.log(quickSort(test1))
 console.log(quickSort(test2))
